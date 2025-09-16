bd0895d5142da4d38fd3d1a8871b14d8003dbe9e
Benjamin Piette <proctar@hotmail.com>
2025-09-16 17:28:31 +0800

    hid kyc-me and elastos dao suggested apps in lightweight mode

--- src/app/dappbrowser/pages/home/suggestedapps.ts
+++ src/app/dappbrowser/pages/home/suggestedapps.ts
@@ -8,16 +8,7 @@ export type DAppMenuEntry = {
   networks: string[]; // List of network keys in which this dapp can run. Empty list = available everywhere.
 };
 
-export const suggestedDApps = (darkMode: boolean): DAppMenuEntry[] => [
-  // {
-  //   icon: '/assets/browser/dapps/feeds.png',
-  //   title: 'Feeds',
-  //   description: 'Feeds is a decentralized social platform where users remain in full control of their data.',
-  //   url: 'https://feeds.feedsnetwork.io/',
-  //   useExternalBrowser: true,
-  //   walletConnectSupported: true,
-  //   networks: ["elastossmartchain"]
-  // },
+export const suggestedDApps = (darkMode: boolean, lightweightMode = false): DAppMenuEntry[] => [
   {
     icon: '/assets/browser/dapps/glidefinance.png',
     title: 'Glide Finance',
@@ -36,42 +27,19 @@ export const suggestedDApps = (darkMode: boolean): DAppMenuEntry[] => [
     walletConnectSupported: true,
     networks: ['elastossmartchain']
   },
-  // {
-  //   icon: '/assets/browser/dapps/chainge.png',
-  //   title: 'Chainge Web dApp',
-  //   description: 'Trade smarter, Not harder.',
-  //   url: 'https://dapp.chainge.finance/',
-  //   useExternalBrowser: false,
-  //   walletConnectSupported: true,
-  //   networks: ["arbitrum", "avalanchecchain", "bsc", "eth", "elastossmartchain", "fusion", "polygon", "tron"]
-  // },
-  // {
-  //   icon: '/assets/browser/dapps/pasar.png',
-  //   title: 'Pasar Protocol',
-  //   description: 'Web3.0 Decentralized Marketplace (DeMKT) and Data Exchange',
-  //   url: 'https://pasarprotocol.io/',
-  //   useExternalBrowser: false,
-  //   walletConnectSupported: true,
-  //   networks: ["elastossmartchain"]
-  // },
-  // {
-  //   icon: '/assets/browser/dapps/meteast.png',
-  //   title: 'MetEast',
-  //   description: 'Decentralized NFT marketplace on Elastos ESC, with better liquidity, autonomous governance and friendly interactions.',
-  //   url: 'https://meteast.io/',
-  //   useExternalBrowser: false,
-  //   walletConnectSupported: true,
-  //   networks: ["elastossmartchain"]
-  // },
-  {
-    icon: '/assets/browser/dapps/elastosdao.svg',
-    title: 'Elastos DAO',
-    description: 'Elastos DAO (DAO) is the community that has naturally formed around Elastos.',
-    url: 'https://elastos.com/',
-    useExternalBrowser: false,
-    walletConnectSupported: true, // Not really, but we can open on ios, as this is a non web3 dapps
-    networks: [] // All networks
-  },
+  ...(!lightweightMode
+    ? [
+        {
+          icon: '/assets/browser/dapps/elastosdao.svg',
+          title: 'Elastos DAO',
+          description: 'Elastos DAO (DAO) is the community that has naturally formed around Elastos.',
+          url: 'https://elastos.com/',
+          useExternalBrowser: false,
+          walletConnectSupported: true, // Not really, but we can open on ios, as this is a non web3 dapps
+          networks: [] // All networks
+        }
+      ]
+    : []),
   {
     icon: '/assets/browser/dapps/anyswap.svg',
     title: 'AnySwap',
@@ -101,41 +69,19 @@ export const suggestedDApps = (darkMode: boolean): DAppMenuEntry[] => [
     walletConnectSupported: true,
     networks: ['bsc', 'elastossmartchain']
   },
-  {
-    icon: '/assets/browser/dapps/kyc-me.png',
-    title: 'Kyc-me',
-    description: 'KYC service for W3C DID Verifiable Credentials on Elastos',
-    url: 'https://kyc-me.io/',
-    useExternalBrowser: false,
-    walletConnectSupported: true,
-    networks: [] // All networks
-  },
-  /* {
-      icon: '/assets/browser/dapps/profile.png',
-      title: 'Profile',
-      description: 'A better way to be online using Elastos DID',
-      url: 'https://profile.site/',
-      useExternalBrowser: false,
-      networks: ["elastossmartchain"]
-  }, */
-  // {
-  //     icon: '/assets/browser/dapps/rocketx.png',
-  //     title: 'RocketX - Skyscanner Crypto',
-  //     description: ' RocketX aggregates Centralised and Decentralised Crypto Exchanges and makes it really simple to trade ANY token listed on ANY exchange. Best rates with minimal slippage.',
-  //     url: 'https://staging.rocketx.exchange/',
-  //     useExternalBrowser: false,
-  //     walletConnectSupported: false,
-  //     networks: ["avalanchecchain", "bsc", "ethereum", "polygon"]
-  // },
-  //   {
-  //     icon: '/assets/browser/dapps/raven.png',
-  //     title: 'Moe Raven',
-  //     description: 'The magical matic yield optimizer',
-  //     url: 'https://raven.moe/',
-  //     useExternalBrowser: false,
-  //     walletConnectSupported: false,
-  //     networks: ["elastossmartchain", "polygon"]
-  //   },
+  ...(!lightweightMode
+    ? [
+        {
+          icon: '/assets/browser/dapps/kyc-me.png',
+          title: 'Kyc-me',
+          description: 'KYC service for W3C DID Verifiable Credentials on Elastos',
+          url: 'https://kyc-me.io/',
+          useExternalBrowser: false,
+          walletConnectSupported: true,
+          networks: [] // All networks
+        }
+      ]
+    : []),
   {
     icon: '/assets/browser/dapps/tokbridge.svg',
     title: 'Shadow Tokens',
@@ -145,24 +91,6 @@ export const suggestedDApps = (darkMode: boolean): DAppMenuEntry[] => [
     walletConnectSupported: false, // Seems to be supported on the website but not working
     networks: ['elastossmartchain', 'bsc', 'ethereum']
   },
-  /* {
-      icon: '/assets/browser/dapps/tin.jpg',
-      title: 'Tin.network',
-      description: 'Manage your DeFi assets and liabilities in one simple interface',
-      url: 'https://tin.network/',
-      useExternalBrowser: false,
-      walletConnectSupported: true, // Not really, but not needed
-      networks: []
-  }, */
-  /* {
-      icon: '/assets/browser/dapps/cryptoname.png',
-      title: 'Cryptoname',
-      description: 'CryptoName is your passport to the crypto world',
-      url: 'https://cryptoname.org/',
-      useExternalBrowser: false,
-      walletConnectSupported: false,
-      networks: ["elastossmartchain"]
-  }, */
   {
     icon: '/assets/browser/dapps/sushiswap.png',
     title: 'Sushiswap',
